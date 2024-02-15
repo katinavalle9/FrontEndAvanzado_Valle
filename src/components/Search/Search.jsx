c248ba50cc09e99df0f53d488e69e1ea64ca5733
katinavalle9 <katinavalle9@gmail.com>
2024-02-14 21:34:21 -0500

    mejoras

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,22 @@
 import { MDBContainer, MDBInputGroup } from "mdb-react-ui-kit";
 import { useSearch } from "../../context/SearchContex";
+import { useState, useEffect } from 'react';
 
 const Search = () => {
   const { setSearchQuery } = useSearch();
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchQuery(inputValue);
+    }, 500); // Debounce por 500 ms
+
+    return () => clearTimeout(timer);
+  }, [inputValue, setSearchQuery]);
 
   const handleSearch = (e) => {
-    e.preventDefault(); 
-    setSearchQuery(e.target.value);
+    e.preventDefault();
+    setInputValue(e.target.value);
   };
 
   return (
@@ -18,6 +28,7 @@ const Search = () => {
           aria-label="Search"
           type="Search"
           onChange={handleSearch}
+          value={inputValue}
         />
       </MDBInputGroup>
     </MDBContainer>
